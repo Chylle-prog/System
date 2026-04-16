ba21eb80ae2d49de9915f8ccaf03a73765bc3531
Chyle <alexie_chyle_magbuhat@dlsl.edu.ph>
2026-04-16 08:58:15 +0800

    iskomats backend 2 Fixes

--- iskomats-applicants-backend/services/auth_service.py
+++ iskomats-applicants-backend/services/auth_service.py
@@ -5,6 +5,7 @@ import re
 DEFAULT_CORS_ORIGINS = (
     "http://localhost:5173,http://localhost:5173/,"
     "http://localhost:3000,http://localhost:3000/,"
+    "http://localhost:3010,http://localhost:3010/,"
     "http://localhost:5174,http://localhost:5174/,"
     "http://localhost:5175,http://localhost:5175/,"
     "https://cozy-kulfi-35f772.netlify.app,"
