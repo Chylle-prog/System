dc511342551ce40d84dc2a946a4da696e04d16e3
Chyle <alexie_chyle_magbuhat@dlsl.edu.ph>
2026-04-17 07:04:45 +0800

    Full Cleanup

--- iskomats-dashboards-thesis-aclm-g3-master/iskomats-admins/TESTPYTHON/Student Ranking/scratch/list_columns.py
+++
@@ -1,7 +0,0 @@
-from services.db_service import get_db
-conn = get_db()
-cur = conn.cursor()
-cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'applicants'")
-columns = [row['column_name'] for row in cur.fetchall()]
-print(f"COLUMNS: {columns}")
-conn.close()
