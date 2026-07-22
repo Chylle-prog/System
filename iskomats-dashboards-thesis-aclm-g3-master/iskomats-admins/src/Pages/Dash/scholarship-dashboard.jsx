71c2dfd5e750ea539377e5feb373f43b07632343
Chyle <alexie_chyle_magbuhat@dlsl.edu.ph>
2026-07-22 12:46:00 +0800

    advanced search removal

--- iskomats-dashboards-thesis-aclm-g3-master/iskomats-admins/src/Pages/Dash/scholarship-dashboard.jsx
+++ iskomats-dashboards-thesis-aclm-g3-master/iskomats-admins/src/Pages/Dash/scholarship-dashboard.jsx
@@ -4862,7 +4862,6 @@ export default function ScholarshipDashboard({
         </header>
 
         {section === 'dashboard' && renderDashboard()}
-        {section === 'advanced-search' && <AdvancedSearch liveScholarships={data.scholarshipPosts} />}
         {section === 'finder' && renderFinder()}
         {section === 'manage' && renderManage()}
         {section === 'track' && renderTrack()}
@@ -5094,6 +5093,7 @@ export default function ScholarshipDashboard({
     </div>
   );
 }
+
 
 
 
